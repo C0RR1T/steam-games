d0019c12522abc5973cfd9e15b4622ec8a95ccea
Corsin <corsin@ragett.li>
2022-09-05 09:09:00 +0200

    New Game Entity

--- src/main/java/ch/bbw/steamgames/Game.java
+++ src/main/java/ch/bbw/steamgames/Game.java
@@ -1,12 +1,37 @@
 package ch.bbw.steamgames;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
 import javax.persistence.Id;
 import java.time.LocalDate;
-import java.util.List;
 
-
-public record Game(@Id Integer appId, String name, LocalDate releaseDate, int english, String developer, String publisher, String platforms, int requiredAge, String categories, String genres, String steamspyTags, int achievements, int positiveRatings, int negativeRatings, int averagePlaytime, int medianPlaytime, String owners, double price) {
+@Entity
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Game {
+    @Id
+    Integer appId;
+    String name;
+    LocalDate releaseDate;
+    int english;
+    String developer;
+    String publisher;
+    String platforms;
+    int requiredAge;
+    String categories;
+    String genres;
+    String steamspyTags;
+    int achievements;
+    int positiveRatings;
+    int negativeRatings;
+    int averagePlaytime;
+    int medianPlaytime;
+    String owners;
+    double price;
 }

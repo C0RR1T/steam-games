7fde0f40ad27b2a59d3ab17ca054f8361db42dfa
Corsin <corsin@ragett.li>
2022-09-05 08:47:02 +0200

    initial commit

---
+++ src/main/java/ch/bbw/steamgames/SteamGamesApplication.java
@@ -0,0 +1,13 @@
+package ch.bbw.steamgames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteamGamesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SteamGamesApplication.class, args);
+    }
+
+}

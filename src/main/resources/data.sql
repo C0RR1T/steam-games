6b1ccc04e797946d738ed5b6f69310afe854565f
Corsin <corsin@ragett.li>
2022-09-05 09:16:02 +0200

    Data sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,21 @@
+-- expects the csv on the classpath (src/main/resources)
+INSERT INTO GAME (APP_ID,
+                  NAME,
+                  RELEASE_DATE,
+                  ENGLISH,
+                  DEVELOPER,
+                  PUBLISHER,
+                  PLATFORMS,
+                  REQUIRED_AGE,
+                  CATEGORIES,
+                  GENRES,
+                  STEAMSPY_TAGS,
+                  ACHIEVEMENTS,
+                  POSITIVE_RATINGS,
+                  NEGATIVE_RATINGS,
+                  AVERAGE_PLAYTIME,
+                  MEDIAN_PLAYTIME,
+                  OWNERS,
+                  PRICE)
+SELECT *
+FROM CSVREAD('classpath:steam.csv')
